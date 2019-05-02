5820e63f1ed05a4baa2b198820a09c711e24bfd0
jon <jon@VMLubuntu18.04>
2019-05-02 14:14:11 +0100

    added new functions

--- problem_solver.rb
+++ problem_solver.rb
@@ -1,5 +1,27 @@
 class ProblemSolver
-	def solve easy problem
+	def solve_easy_problem
+	end
+
+	def solve_harder_problem
+	end
+
+	def solve_tough_problem
+	end
+
+	def solve_hill_climb_problem
+		hill_climb = HillClimb.new
+		hill_climb.climb
+	end
+
+	def use_generic_algorithm(problem)
+		darwin = Darwin.new(problem)
+		darwin.survive
+	end
+
+	def prolog_solver(descr, ques)
+		prolog = Prolog.new
+		prolog.describe_world(descr)
+		prolog.as(ques)
 	end
 end
 
